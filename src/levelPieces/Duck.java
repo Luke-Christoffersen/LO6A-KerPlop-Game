1e1ec87ac8a4675c297f8ccd31891288d5f0114f
Aidan Schiefer <aidanschiefer@gmail.com>
2026-09-23 15:25:28 -0600

    Added draw functionality, imported random package, and added TODO comments for interact and move

--- src/levelPieces/Duck.java
+++ src/levelPieces/Duck.java
@@ -1,6 +1,7 @@
 package levelPieces;
 
 import gameEngine.Drawable;
+import java.util.random.*;
 import gameEngine.Moveable;
 import gameEngine.InteractionResult;
 
@@ -18,8 +19,34 @@ public class Duck extends GamePiece implements Drawable, Moveable{
 	}
 	
 	@Override
-	// Overridden interact method. The duck moves around randomly and if it is in the same square as the player, it moves the player to a random location
+	/*
+	 * Overridden interact method from GamePiece. The duck moves around randomly and if it is in the same square as the player, it moves the player to a random location
+	 * 
+	 * @param gameBoard: An array of drawable symbols that makes up the game board
+	 * @param playerLocation: Integer value representing the location of the duck
+	 * */ 
 	public InteractionResult interact(Drawable[] gameboard, int playerLocation) {
+		// @TODO: Implement duck-player interaction. Generate a random number between 1 and 3 inclusive. 
+		// If 1: The player gets a point
+		// If 2: The player gets a point
+		// If 3: The player gets to advance
 		return InteractionResult.NONE;
+	}
+	
+	@Override
+	// Overridden draw method from Drawable. Prints out the symbol of the duck
+	public void draw() {
+		System.out.print(this.symbol);
+	}
+	
+	@Override
+	/*
+	 * Overridden move method from Moveable. Moves the duck to a random position on the board
+	 * 
+	 * @param gameBoard: An array of drawable symbols that makes up the game board
+	 * @param playerLocation: Integer value representing the location of the duck
+	 * */
+	public void move(Drawable[] gameBoard, int playerLocation) {
+		// @TODO: Have the duck move to a random location
 	}
 }
