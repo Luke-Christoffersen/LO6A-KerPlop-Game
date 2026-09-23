d64bdd64042253c35bcadc421eda7531d4815eea
Aidan Schiefer <aidanschiefer@gmail.com>
2026-09-23 12:50:34 -0600

    Initial shell for Duck game piece

---
+++ src/levelPieces/Duck.java
@@ -0,0 +1,24 @@
+package levelPieces;
+
+import gameEngine.Drawable;
+import gameEngine.InteractionResult;
+
+public class Duck extends GamePiece{
+	
+	/*
+	 * Constructor for a Duck moving game piece. This will be the randomly moving piece
+	 * 
+	 * @param symbol: Symbol that represents the duck ('D')
+	 * @param label
+	 * @param location: The value holding the location of the duck
+	 * */
+	public Duck(char symbol, String label, int location) {
+		super(symbol, label, location);
+	}
+	
+	@Override
+	// Overridden interact method. The duck moves around randomly and if it is in the same square as the player, it moves the player to a random location
+	public InteractionResult interact(Drawable[] gameboard, int playerLocation) {
+		return InteractionResult.NONE;
+	}
+}
