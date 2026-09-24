fa2b2cc772743efae2dc7220059b2efc25a33ee1
Your Name <luke_christoffersen@mines.edu>
2026-09-24 17:43:05 -0600

    feat: added constructor, createLevel, and getBoard implementation for LevelSetup

--- src/levelPieces/LevelSetup.java
+++ src/levelPieces/LevelSetup.java
@@ -1,20 +1,85 @@
 package levelPieces;
 
 import java.util.ArrayList;
-
 import gameEngine.Drawable;
+import gameEngine.GameEngine;
 import gameEngine.Moveable;
 
 public class LevelSetup {
+	
+	
+	private Drawable[] board;
+	private ArrayList<Moveable> movingPieces;
+	private ArrayList<GamePiece> interactingPieces;
+	private int playerStartLoc;
+	
+	public LevelSetup() {
+		board = new Drawable[GameEngine.BOARD_SIZE];
+		movingPieces = new ArrayList<>();
+		interactingPieces = new ArrayList<>();
+		playerStartLoc = 0;
+	}
 
 	public void createLevel(int levelNum) {
-		Drawable[] Board = new Drawable[levelNum];
+		board = new Drawable[GameEngine.BOARD_SIZE];
+		movingPieces = new ArrayList<>();
+		interactingPieces = new ArrayList<>();
 		
+		if (levelNum == 1) {
+			playerStartLoc = 10;
+
+			Coin coin0 = new Coin('C', "Coin", 1, false);
+			Coin coin1 = new Coin('C', "Coin", 9, false);
+			Goal goal = new Goal('X', "Goal", 18);
+			SpikePit pit = new SpikePit('^', "Spike Pit", 7);
+			SpinningTrap spinner = new SpinningTrap('-', "Spinning Trap", 5);
+			Wind wind = new Wind();
+			
+
+			board[coin0.getLocation()] = coin0;
+			board[coin1.getLocation()] = coin1;
+			board[goal.getLocation()] = goal;
+			board[pit.getLocation()] = pit;
+			board[spinner.getLocation()] = spinner;
+			board[12] = wind;
+
+			interactingPieces.add(coin0);
+			interactingPieces.add(coin1);
+			interactingPieces.add(goal);
+			interactingPieces.add(pit);
+			interactingPieces.add(spinner);
+		}
+		else if (levelNum == 2) {
+			playerStartLoc = 10;
+
+			Coin coin0 = new Coin('C', "Coin", 1, false);
+			Coin coin1 = new Coin('C', "Coin", 16, false);
+			Goal goal = new Goal('X', "Goal", 18);
+			SpikePit pit = new SpikePit('^', "Spike Pit", 7);
+			Goblin goblin = new Goblin('G', "Goblin", 14);
+			Duck duck = new Duck('D', "Duck", 4);
+			Wind wind = new Wind();
+			
+
+			board[coin0.getLocation()] = coin0;
+			board[coin1.getLocation()] = coin1;
+			board[goal.getLocation()] = goal;
+			board[pit.getLocation()] = pit;
+			board[goblin.getLocation()] = goblin;
+			board[duck.getLocation()] = duck;
+			board[12] = wind;
+
+			interactingPieces.add(coin0);
+			interactingPieces.add(coin1);
+			interactingPieces.add(goal);
+			interactingPieces.add(pit);
+			interactingPieces.add(goblin);
+			interactingPieces.add(duck);
+		}
 	}
 
 	public Drawable[] getBoard() {
-		// TODO Auto-generated method stub
-		return null;
+		return board;
 	}
 
 	public ArrayList<Moveable> getMovingPieces() {
