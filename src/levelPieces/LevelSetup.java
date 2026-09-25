faf3d731e760709b28628c93586ec711311c0677
Aidan Schiefer <aidanschiefer@gmail.com>
2026-09-24 18:02:45 -0600

    feat: finsihed LevelSetup implementation and added extra game pieces to the level setup

--- src/levelPieces/LevelSetup.java
+++ src/levelPieces/LevelSetup.java
@@ -32,6 +32,8 @@ public class LevelSetup {
 			Coin coin1 = new Coin('C', "Coin", 9, false);
 			Goal goal = new Goal('X', "Goal", 18);
 			SpikePit pit = new SpikePit('^', "Spike Pit", 7);
+			Duck duck1 = new Duck('D', "Duck", 15);
+			Duck duck2 = new Duck('D', "Duck", 12);
 			SpinningTrap spinner = new SpinningTrap('-', "Spinning Trap", 5);
 			Wind wind = new Wind();
 			
@@ -43,11 +45,16 @@ public class LevelSetup {
 			board[spinner.getLocation()] = spinner;
 			board[12] = wind;
 
+			// Add the interacting pieces to the list
 			interactingPieces.add(coin0);
 			interactingPieces.add(coin1);
 			interactingPieces.add(goal);
 			interactingPieces.add(pit);
 			interactingPieces.add(spinner);
+			
+			// Add the moving pieces to the list
+			movingPieces.add(duck1);
+			movingPieces.add(duck2);
 		}
 		else if (levelNum == 2) {
 			playerStartLoc = 10;
@@ -68,13 +75,18 @@ public class LevelSetup {
 			board[goblin.getLocation()] = goblin;
 			board[duck.getLocation()] = duck;
 			board[12] = wind;
-
+			
+			// Add the interacting pieces to the list
 			interactingPieces.add(coin0);
 			interactingPieces.add(coin1);
 			interactingPieces.add(goal);
 			interactingPieces.add(pit);
 			interactingPieces.add(goblin);
 			interactingPieces.add(duck);
+			
+			// Add the moveable pieces to the list
+			movingPieces.add(duck);
+			movingPieces.add(goblin);
 		}
 	}
 
@@ -83,18 +95,18 @@ public class LevelSetup {
 	}
 
 	public ArrayList<Moveable> getMovingPieces() {
-		// TODO Auto-generated method stub
-		return null;
+		// Return the list of moving pieces
+		return movingPieces;
 	}
 
 	public ArrayList<GamePiece> getInteractingPieces() {
-		// TODO Auto-generated method stub
-		return null;
+		// Return the list of interactive pieces
+		return interactingPieces;
 	}
 
 	public int getPlayerStartLoc() {
-		// TODO Auto-generated method stub
-		return 0;
+		// Return the player start location
+		return playerStartLoc;
 	}
 
 }
